c223ff749fc8c0b84d600d637a39e74287c26fd6
worgflores <worgflores@gmail.com>
2021-02-10 20:38:18 +0800

    _res: patches: added

---
+++ _res/patches/v255_20210201.patch/mysql/02-patch-etracs.sql
@@ -0,0 +1,97 @@
+drop table if exists sync_data_pending
+;
+drop table if exists sync_data_forprocess
+;
+drop table if exists sync_data
+;
+
+
+CREATE TABLE `rpt_syncdata_forsync` (
+  `objid` varchar(50) NOT NULL,
+  `reftype` varchar(50) NOT NULL,
+  `refno` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `orgid` varchar(50) NOT NULL,
+  `dtfiled` datetime NOT NULL,
+  `createdby_objid` varchar(50) DEFAULT NULL,
+  `createdby_name` varchar(255) DEFAULT NULL,
+  `createdby_title` varchar(50) DEFAULT NULL,
+  `remote_orgid` varchar(15) DEFAULT NULL,
+  `state` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_refno` (`refno`),
+  KEY `ix_orgid` (`orgid`),
+  KEY `ix_state` (`state`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+CREATE TABLE `rpt_syncdata` (
+  `objid` varchar(50) NOT NULL,
+  `state` varchar(25) NOT NULL,
+  `refid` varchar(50) NOT NULL,
+  `reftype` varchar(50) NOT NULL,
+  `refno` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `dtfiled` datetime NOT NULL,
+  `orgid` varchar(50) NOT NULL,
+  `remote_orgid` varchar(50) DEFAULT NULL,
+  `remote_orgcode` varchar(5) DEFAULT NULL,
+  `remote_orgclass` varchar(25) DEFAULT NULL,
+  `sender_objid` varchar(50) DEFAULT NULL,
+  `sender_name` varchar(255) DEFAULT NULL,
+  `sender_title` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_state` (`state`),
+  KEY `ix_refid` (`refid`),
+  KEY `ix_refno` (`refno`),
+  KEY `ix_orgid` (`orgid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+CREATE TABLE `rpt_syncdata_error` (
+  `objid` varchar(50) NOT NULL,
+  `filekey` varchar(1000) NOT NULL,
+  `error` text,
+  `refid` varchar(50) NOT NULL,
+  `reftype` varchar(50) NOT NULL,
+  `refno` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `idx` int(11) NOT NULL,
+  `info` text,
+  `parent` text,
+  `remote_orgid` varchar(50) DEFAULT NULL,
+  `remote_orgcode` varchar(5) DEFAULT NULL,
+  `remote_orgclass` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_refid` (`refid`),
+  KEY `ix_refno` (`refno`),
+  KEY `ix_filekey` (`filekey`(255)),
+  KEY `ix_remote_orgid` (`remote_orgid`),
+  KEY `ix_remote_orgcode` (`remote_orgcode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+CREATE TABLE `rpt_syncdata_item` (
+  `objid` varchar(50) NOT NULL,
+  `parentid` varchar(50) NOT NULL,
+  `state` varchar(25) NOT NULL,
+  `refid` varchar(50) NOT NULL,
+  `reftype` varchar(50) NOT NULL,
+  `refno` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `idx` int(11) NOT NULL,
+  `info` text,
+  PRIMARY KEY (`objid`),
+  KEY `ix_parentid` (`parentid`),
+  KEY `ix_state` (`state`),
+  KEY `ix_refid` (`refid`),
+  KEY `ix_refno` (`refno`),
+  CONSTRAINT `FK_parentid_rpt_syncdata` FOREIGN KEY (`parentid`) REFERENCES `rpt_syncdata` (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+
+INSERT INTO `sys_var` (`name`, `value`, `description`, `datatype`, `category`) VALUES ('fileserver_upload_task_active', '0', 'Activate / Deactivate upload task', 'boolean', 'SYSTEM')
+;
+
+
